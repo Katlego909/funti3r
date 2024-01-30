34283cf1d56e237628ff1f7606b07e9a64a44b95
Katlego909 <67097039+Katlego909@users.noreply.github.com>
2024-01-30 09:00:03 +0200

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -113,11 +113,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 30,
-            # Disable deterministic ordering
-            'deterministic': False,
-        },
+        # 'OPTIONS': {
+        #     'timeout': 30,
+        #     # Disable deterministic ordering
+        #     'deterministic': False,
+        # },
     }
 }
 
